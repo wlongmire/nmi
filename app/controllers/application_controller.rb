8efa1fa0289247395f9ff506256f96abef29f704
Warren Longmire <warrenlongmire@gmail.com>
2016-03-29 10:52:29 -0400

    adding user controllers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,21 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
+
+  	protected
+
+	def configure_devise_permitted_parameters
+		registration_params = [:name, :username, :password, :password_confirmation]
+
+	    if params[:action] == 'update'
+	      devise_parameter_sanitizer.for(:account_update) { 
+	        |u| u.permit(registration_params << :current_password)
+	      }
+	    elsif params[:action] == 'create'
+	      devise_parameter_sanitizer.for(:sign_up) { 
+	        |u| u.permit(registration_params) 
+	      }
+		end
+	end
+
 end
