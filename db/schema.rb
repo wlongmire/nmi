6cca9696458c8ea88f44404cf4be99d1a7085bda
Warren Longmire <warrenlongmire@gmail.com>
2016-03-31 12:04:11 -0400

    adding migration for username.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329162730) do
+ActiveRecord::Schema.define(version: 20160331155655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
