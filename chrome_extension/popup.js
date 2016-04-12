ca2340c0ee378dbb4f09d3edda8d2c98f3e3f297
Warren Longmire <warrenlongmire@gmail.com>
2016-04-11 23:42:39 -0400

    adding id for extention.

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -2,19 +2,13 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
-    chrome.tabs.executeScript({
-   		code: 'document.getElementById("new_url").value = "' + url + '"'
+    
+    chrome.tabs.create({ url: "https://nmiresearcher.herokuapp.com/link", active:true}, function(){
+    	chrome.tabs.executeScript({
+   			code: 'document.getElementById("new_url").value = "' + url + '"; document.forms["loginForm"].submit();'
+  		});
   	});
+
 	});
 
 });
-
-// chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-// 		var url = tabs[0].url;
-
-// 		var newURL = "https://nmiresearcher.herokuapp.com/link";
-
-// 			alert("here");
-// 		});
-
-// });
