b8846f8ba11b91f5ffc0a3814aa95fe1e9b773d3
Warren Longmire <warrenlongmire@gmail.com>
2016-04-12 01:04:00 -0400

    adding chrome extension download.

--- nmi_extension/popup.js
+++ nmi_extension/popup.js
@@ -4,6 +4,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     var url = tabs[0].url;
     
     chrome.tabs.create({ url: "https://nmiresearcher.herokuapp.com/link", active:true}, function(){
+    	
     	chrome.tabs.executeScript({
    			code: 'document.getElementById("new_url").value = "' + url + '"; document.forms["new_form"].submit();'
   		});
